e1e7c4448f39bfa9fa5d09f586985d5bbe454ac8
doganoguz <info@doganoguz.com>
2022-06-21 16:55:51 +0300

    font added

---
+++ lib/page/onboarding/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatefulWidget {
+  OnBoarding({Key? key}) : super(key: key);
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
