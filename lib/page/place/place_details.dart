7df7d643f3e96420fda0091ab08fec7f73abff18
doganoguz <info@doganoguz.com>
2022-07-27 11:44:38 +0300

    place details added

--- lib/page/place/place_details.dart
+++ lib/page/place/place_details.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class PlaceDetails extends StatefulWidget {
@@ -8,6 +10,15 @@ class PlaceDetails extends StatefulWidget {
 }
 
 class _PlaceDetailsState extends State<PlaceDetails> {
+  final sikiciSayac = 10;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print(sikiciSayac + 10);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
